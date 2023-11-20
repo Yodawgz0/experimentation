4d3658c8a752457b0490c8a97ae342410b4d625d
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-11-20 09:57:35 -0500

    adding search to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState, createContext } from "react";
 import { Acomp } from "./extraComp/Acomp";
+import { Search } from "./extraComp/Search";
 import "./styles.css";
 interface props {
   title: string;
@@ -16,34 +17,53 @@ function Movie({ title, releaseDate, memo }: props) {
   );
 }
 
-export const AcompContext = createContext();
+export const AcompContext = createContext<{
+  numCHange: number;
+  setNum: React.Dispatch<React.SetStateAction<number>>;
+}>({
+  numCHange: 0,
+  setNum: () => {},
+});
 
 const MemoizedMovie = React.memo(Movie);
 
 export default function App() {
   const [title, setTitle] = useState<string>("Hear");
   const [numCHange, setNum] = useState<number>(5);
-
+  const [firstName, setFirstName] = useState<string>("");
+  const [secondName, setSecondName] = useState<string>("");
   const handleChange = useCallback(() => {
     setTitle(title + "2");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [numCHange]);
+
+  const fullName = firstName + " " + secondName;
   return (
     <>
       <AcompContext.Provider value={{ numCHange, setNum }}>
-        <MemoizedMovie
-          title={title}
-          releaseDate="December 15, 1995"
-          memo={true}
-        />
-        <Movie title={title} releaseDate="December 15, 1995" memo={false} />
-        <button onClick={() => handleChange()}>Check ifnum updated</button>
-        <button onClick={() => setNum(numCHange + 1)}>
-          Enable UseCallback
-        </button>
-
         <Acomp />
-        <button>{numCHange}</button>
+        <p> This is the incremented {numCHange}</p>
       </AcompContext.Provider>
+      <MemoizedMovie
+        title={title}
+        releaseDate="December 15, 1995"
+        memo={true}
+      />
+      <Movie title={title} releaseDate="December 15, 1995" memo={false} />
+
+      <button onClick={() => handleChange()}>Check ifnum updated</button>
+      <button onClick={() => setNum(numCHange + 1)}>Enable UseCallback</button>
+      {/* fact that we don't need useeffect */}
+      <div className="d-flex flex-row mb-2 mt-4">
+        <p className="me-5">First name: </p>
+        <input onChange={(e) => setFirstName(e.target.value)} />
+      </div>
+      <div className="d-flex flex-row">
+        <p className="me-5">Last name: </p>
+        <input onChange={(e) => setSecondName(e.target.value)} />
+      </div>
+      <p>{fullName}</p>
+      <Search />
     </>
   );
 }
