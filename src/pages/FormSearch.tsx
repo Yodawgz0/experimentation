28f3e3204f0262c73498ab343bdb209ddbda9476
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2024-01-03 20:47:36 -0500

    addind searchquery params

--- src/pages/FormSearch.tsx
+++ src/pages/FormSearch.tsx
@@ -2,11 +2,37 @@ import React from "react";
 import { useSearchParams } from "react-router-dom";
 
 const FormSearch = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams({ search: "" });
+  const searchVal = searchParams.get("search");
+  console.log(searchVal);
+  const searchItems: string[] = ["Computer", "Keyboard", "Mouse", "Light"];
   return (
     <>
-      <div>
-        <input onChange={(e) => setSearchParams(e.target.value)} />
+      <div className="p-4">
+        <div className=" d-flex flex-row">
+          <p className="me-4">Title</p>
+          <input
+            onChange={(e) =>
+              setSearchParams(
+                (prev) => {
+                  prev.set("search", e.target.value);
+                  return prev;
+                },
+                { replace: true }
+              )
+            }
+          />
+        </div>
+
+        <ul>
+          {searchItems.map((e) =>
+            e.toLowerCase().includes(searchVal!.toLowerCase()) ? (
+              <li key={e}>{e}</li>
+            ) : (
+              <></>
+            )
+          )}
+        </ul>
       </div>
     </>
   );
